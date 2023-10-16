a821196f0cb1757ce1a95a2e24c2f768ab759882
Fram3dL00neyJ <47389693+Fram3dL00neyJ@users.noreply.github.com>
2023-10-16 13:09:53 +0200

    Update contact_me.js

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "https://79r7ant3fe.execute-api.us-east-1.amazonaws.com/LambdaFunction",
+                url: "https://5eoopag4ui.execute-api.us-east-1.amazonaws.com/default/TestLambda",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
