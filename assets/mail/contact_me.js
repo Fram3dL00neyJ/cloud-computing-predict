ed07e2af56a4c34037d39f1ccf496566f27dc13c
Fram3dL00neyJ <47389693+Fram3dL00neyJ@users.noreply.github.com>
2023-10-16 12:28:17 +0200

    Update contact_me.js

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "https://main.d24soq134dh9ix.amplifyapp.com/sendmessage_api",
+                url: "https://79r7ant3fe.execute-api.us-east-1.amazonaws.com/LambdaFunction",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
